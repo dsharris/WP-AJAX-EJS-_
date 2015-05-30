e4ff67c2923e70a750aad4437266d711c79803dc
Quinton Aiken <quintonlaiken@gmail.com>
2015-05-29 20:50:54 -0500

    add example case

--- wp-content/themes/wp-theme/infinite-scroll.php
+++ wp-content/themes/wp-theme/infinite-scroll.php
@@ -1,10 +1,35 @@
 <?php 
-    get_header();
-    // Template Name: Infinite Scroll
-
+// Template Name: Infinite Scroll
+get_header();
 ?>
 
+<?php 
+// Example Usage for a Blog Home Page
 
-<section class="infinite-scroll" data-action="load_more_posts" data-template="posts.ejs"></section>
+// Templates found in functions/wordpress/ajax/templates
+// and correspond to data-template attr on section tag.
+
+// data-action attr corresponds to function called in
+// functions/wordpress/ajax/infinite-scroll.php
+?>
+
+<section class="infinite-scroll" data-action="load_more_posts" data-template="posts.ejs">
+
+  <?php if (have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+    <article>
+
+      <h2><?php the_title(); ?></h2>
+      <p><?php the_content(); ?></p>
+
+    </article>
+
+  <?php endwhile; else : ?>
+
+    <p>There are no posts or pages here.</p>
+
+  <?php endif; ?>
+
+</section>
 
 <?php get_footer(); ?>
